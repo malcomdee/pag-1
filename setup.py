2aea17ad11935ef5cb574d9acd2b5196df5efb74
malcomdee <89889959+malcomdee@users.noreply.github.com>
2021-11-18 01:45:00 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,24 @@ from flask import Flask, render_template, request, redirect, session, flash, jso
 app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe'
 
+from decouple import config as config_decouple
 
+def create_app(enviroment):
+    app = Flask(__name__)
+
+    app.config.from_object(enviroment)
+
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
+    return app
+
+enviroment = config['development']
+if config_decouple('PRODUCTION', default=False):
+    enviroment = config['production']
+
+app = create_app(enviroment)
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
